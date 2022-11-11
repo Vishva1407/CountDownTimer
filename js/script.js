8bae21548da713466fa67932d5b2f23b47ce46f7
Vishva1407 <mvishva1407@gmail.com>
2022-11-11 09:04:15 +0530

    Commit all

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function updateTimer()
+{
+const currentYear = new Date().getFullYear();
+const newYear =new Date(`January 1 ${currentYear+1} 00:00:00:00`);
+const diff = newYear - new Date();
+const days = Math.floor(diff/1000/60/60/24);
+const hours = Math.floor((diff/1000/60/60)%24);
+const minutes = Math.floor((diff/1000/60)%60);
+const seconds = Math.floor((diff/1000)%60);
+const daysQuery = document.querySelector("#days");
+const hoursQuery = document.querySelector("#hours");
+const minutesQuery = document.querySelector("#minutes");
+const secondsQuery = document.querySelector("#seconds");
+daysQuery.innerHTML = days;
+hoursQuery.innerHTML = hours;
+minutesQuery.innerHTML = minutes;
+secondsQuery.innerHTML = seconds;
+}
+
+setInterval(updateTimer,1000);
